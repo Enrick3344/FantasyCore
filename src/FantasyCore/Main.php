b800d61d1a0652b9dddf2dc9cff6b7d43c2cf2f0
Enrick3344 <enrico3815@hotmail.fr>
2017-10-01 20:06:12 -0400

    Update Main.php

--- src/FantasyCore/Main.php
+++ src/FantasyCore/Main.php
@@ -1,3 +1,4 @@
+<?php
 /*
  *
  *  _____           _                   ____               
@@ -5,7 +6,7 @@
  * | |_ / _` | '_ \| __/ _` / __| | | | |   / _ \| '__/ _ \
  * |  _| (_| | | | | || (_| \__ \ |_| | |__| (_) | | |  __/
  * |_|  \__,_|_| |_|\__\__,_|___/\__, |\____\___/|_|  \___|
- * 								 |___/                     
+ * 				 |___/                     
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
